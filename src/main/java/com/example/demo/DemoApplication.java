661816443af5e9ef73c43e240139056b9052f925
Brahma behera <beherabrahmananda77@gmail.com>
2024-12-14 12:27:11 +0530

    new commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("NEwbranch commits");
+		System.out.println("giii");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
