479dd26c25228d09d2ddc688b576f70b769ff774
Brahma behera <beherabrahmananda77@gmail.com>
2024-12-14 18:08:47 +0530

    new change balunga

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("NEwbranch commits");
 		System.out.println("giii");
-		System.out.println("Chilika");
+		System.out.println("Balunga");
 		System.out.println("Puri");
 		SpringApplication.run(DemoApplication.class, args);
 	}
